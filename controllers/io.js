349b96dcd072a7874e85e010fe6a225c2dc5ec74
Liuchenling <wssgcg1213@qq.com>
2015-04-23 15:18:51 +0800

    fix

--- controllers/io.js
+++ controllers/io.js
@@ -83,7 +83,8 @@ module.exports = function (io){
         });
         socket.timer = setInterval(function() {
             getScreenData(function(data){
-                if(!compareData(data, oldData)){
+                if(!compareData(data, oldData)){//如果改变
+                    oldData = data;
                     socket.emit('queryReturn', data);
                 }
             });
