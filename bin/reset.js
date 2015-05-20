b25f8fa2cc3a9d89cc19db0bddcc563f98d36b12
Liuchenling <wssgcg1213@qq.com>
2015-05-20 15:29:18 +0800

    ok

--- bin/reset.js
+++ bin/reset.js
@@ -1,10 +1,19 @@
 #!/usr/bin/env node
 
 var fs = require('fs'),
-    voterModel = require('../models/voters');
+    voterModel = require('../models/voters'),
+    candidatesModel = require('../models/candidates');
+
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://wssgcg1213:6884650@localhost:27017/wsvoter');
 
-voterM
+voterModel.update({}, {record: "[]"}, { multi: true }, function(err, doc){
+    if(err) return console.log("err", err);
+    console.log(doc.uniqueid);
+});
 
+candidatesModel.update({}, {voteNumber: 0}, { multi: true }, function(err, doc){
+    if(err) return console.log("err", err);
+    console.log(doc.name);
+});
 
