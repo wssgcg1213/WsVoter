60a6ecd99981e0a9c370e8877e6a15a5543918c5
Liuchenling <wssgcg1213@qq.com>
2015-05-20 15:07:12 +0800

    add reset

---
+++ bin/reset.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+var fs = require('fs'),
+    voterModel = require('../models/voters');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://wssgcg1213:6884650@localhost:27017/wsvoter');
+
+voterM
+
+
